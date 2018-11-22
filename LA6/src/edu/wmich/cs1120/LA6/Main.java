5da6239e3f5ac55fd2503f948483f09bf46b025f
Admin <Admin@DESKTOP-TC0LONV>
2018-11-21 19:04:29 -0500

    Andy - Interfaces

---
+++ LA6/src/edu/wmich/cs1120/LA6/Main.java
@@ -0,0 +1,17 @@
+package edu.wmich.cs1120.LA6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IEncoder encoder = new Encoder();
+		IDecoder decoder = new Decoder();
+		String inputFileName = "input.txt";
+		String encodedFileName = inputFileName+".encode";
+		encoder.encode(inputFileName,encodedFileName);
+		decoder.decode(encodedFileName);
+
+
+	}
+
+}
